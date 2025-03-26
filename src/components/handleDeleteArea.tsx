7cb7041b8aa06eceed3624c77245d905d33085c2
Monique de Souza <moniquejp16@gmail.com>
2025-03-26 13:00:39 -0300

    feat:request for area exclusion order

--- src/components/handleDeleteArea.tsx
+++ src/components/handleDeleteArea.tsx
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-export function handleDeleteArea(processId: string, authToken: string) {
-    const id = processId
-    // const urlCode = `${import.meta.env.VITE_API_URL}/tree/deleteProcess/${id}`;
-    // axios.delete(urlCode, {
-    //     headers: { Authorization: `Bearer ${authToken}` }
-    // })
-    //     .then((response) => {
-    //         alert("Exclusão realizada com sucesso");
-    //         window.location.reload();
-    //     })
-    //     .catch((err) => {
-    //         console.error('Erro no registro:', err);
-    //         if (err.response) {
-    //             alert(err.response.data || 'Erro ao registrar. Tente novamente.');
-    //         } else if (err.request) {
-    //             alert('Sem resposta do servidor. Verifique sua conexão.');
-    //         } else {
-    //             alert('Erro ao configurar a requisição.');
-    //         }
-    //     })
+export function handleDeleteArea(areaId: number, authToken: string) {
+    const id = areaId
+    const urlCode = `${import.meta.env.VITE_API_URL}/tree/deleteArea/${id}`;
+    axios.delete(urlCode, {
+        headers: { Authorization: `Bearer ${authToken}` }
+    })
+        .then((response) => {
+            alert("Exclusão realizada com sucesso");
+            window.location.reload();
+        })
+        .catch((err) => {
+            console.error('Erro no registro:', err);
+            if (err.response) {
+                alert(err.response.data || 'Erro ao registrar. Tente novamente.');
+            } else if (err.request) {
+                alert('Sem resposta do servidor. Verifique sua conexão.');
+            } else {
+                alert('Erro ao configurar a requisição.');
+            }
+        })
 };
